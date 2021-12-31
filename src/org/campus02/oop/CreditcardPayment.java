1650ccf96314e8abe383277bfbd5257f6e796abe
Hassan-Erfani <erfani.mojtaba@hotmail.com>
2021-12-31 23:40:03 +0100

    CreditcardPayment() => done

--- src/org/campus02/oop/CreditcardPayment.java
+++ src/org/campus02/oop/CreditcardPayment.java
@@ -1,4 +1,34 @@
 package org.campus02.oop;
 
 public class CreditcardPayment extends Payment{
+    private String cardNumber;
+    private String securityCode;
+
+    public CreditcardPayment(double amount, String currency, String cardNumber, String securityCode) {
+        super(amount, currency);
+        this.cardNumber = cardNumber;
+        this.securityCode = securityCode;
+    }
+
+    public double calcTransactionCosts() {
+        if(getCurrency().equals("DOGE")) {
+            return 0;
+        }
+        if (!getCurrency().equals("EUR")) {
+            return 1.5 + getAmount() * 2.75;
+        } else {
+            return 0.5 + getAmount() * 1.1;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "CreditcardPayment{" +
+                "cardNumber='" + cardNumber + '\'' +
+                ", securityCode='" + securityCode + '\'' +
+                ", Amount='" + getAmount() + '\'' +
+                ", Currency='" + getCurrency() + '\'' +
+                ", TransactionCosts='" + calcTransactionCosts() + '\'' +
+                '}';
+    }
 }
