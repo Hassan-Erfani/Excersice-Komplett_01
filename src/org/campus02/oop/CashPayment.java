838106151894b0a053a611d2c31f23bef19c1165
Hassan-Erfani <erfani.mojtaba@hotmail.com>
2021-12-31 23:41:01 +0100

    CashPayment() => done

--- src/org/campus02/oop/CashPayment.java
+++ src/org/campus02/oop/CashPayment.java
@@ -1,4 +1,31 @@
 package org.campus02.oop;
 
 public class CashPayment extends Payment{
+    private String name;
+
+    public CashPayment(double amount, String currency) {
+        super(amount, currency);
+    }
+
+    public CashPayment(String person, double amount, String currency) {
+        super(amount, currency);
+        this.name = person;
+    }
+
+    public double calcTransactionCosts() {
+        if (this.name.isEmpty()) {
+            return 2;
+        }
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return "CashPayment{" +
+                "name='" + name + '\'' +
+                "Amount='" + getAmount() + '\'' +
+                "Currency='" + getCurrency() + '\'' +
+                "TransactionCosts='" + calcTransactionCosts() + '\'' +
+                '}';
+    }
 }
