209828a3d041b3e8b292bc91ad2083c601e617d0
Hassan-Erfani <erfani.mojtaba@hotmail.com>
2021-12-31 23:40:30 +0100

    MaestroPayment() => done

--- src/org/campus02/oop/MaestroPayment.java
+++ src/org/campus02/oop/MaestroPayment.java
@@ -1,4 +1,29 @@
 package org.campus02.oop;
 
 public class MaestroPayment extends Payment{
+    private String cardNumber;
+
+    public MaestroPayment(double amount, String currency, String cardNumber) {
+        super(amount, currency);
+        this.cardNumber = cardNumber;
+    }
+
+    public double calcTransactionCosts() {
+        if(getCurrency().equals("DOGE")) {
+            return getAmount();
+        } else if ( (getAmount() * 0.75) > 0.95 ) {
+            return getAmount() * 0.75;
+        }
+        return 0.95;
+    }
+
+    @Override
+    public String toString() {
+        return "MaestroPayment{" +
+                "cardNumber='" + cardNumber + '\'' +
+                "Amount='" + getAmount() + '\'' +
+                "Currency='" + getCurrency() + '\'' +
+                "TransactionCosts='" + calcTransactionCosts() + '\'' +
+                '}';
+    }
 }
