7a424c86e2e32e1106c222dd6f6d1e9f4b2c4f93
Hassan-Erfani <erfani.mojtaba@hotmail.com>
2022-01-01 16:54:51 +0100

    isSortedArray()

--- src/arrays/NumberHelper.java
+++ src/arrays/NumberHelper.java
@@ -1,4 +1,21 @@
 package arrays;
 
+import java.util.Arrays;
+
 public class NumberHelper {
+
+    public static void main(String[] args) {
+        int[] arr = {1,3,3,5,6,6};
+        System.out.println(isSortedArray(arr));
+    }
+
+    public static boolean isSortedArray(int[] numbers) {
+        for (int i = 0; i < numbers.length-1; i++) {
+            if (numbers[i] > numbers[i + 1])
+                return false;
+        }
+        return true;
+    }
 }
+
+
