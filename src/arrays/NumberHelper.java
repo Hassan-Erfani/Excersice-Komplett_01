ca665f17993deaace5c6114878beca4efeda0fad
Hassan-Erfani <erfani.mojtaba@hotmail.com>
2022-01-01 16:55:56 +0100

    removeDuplicates()

--- src/arrays/NumberHelper.java
+++ src/arrays/NumberHelper.java
@@ -7,6 +7,7 @@ public class NumberHelper {
     public static void main(String[] args) {
         int[] arr = {1,3,3,5,6,6};
         System.out.println(isSortedArray(arr));
+        System.out.println(Arrays.toString(removeDuplicates(arr)));
     }
 
     public static boolean isSortedArray(int[] numbers) {
@@ -15,6 +16,17 @@ public class NumberHelper {
                 return false;
         }
         return true;
+    }
+
+    public static int[] removeDuplicates(int[] numbers) {
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = i + 1; j < numbers.length; j++) {
+                if (numbers[i] == numbers[j]) {
+                    numbers[i] = -1;
+                }
+            }
+        }
+        return numbers;
     }
 }
 
