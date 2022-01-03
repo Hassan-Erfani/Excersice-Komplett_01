21698c78cb047f5da166e0f0246620bfbf3c2b06
Hassan-Erfani <erfani.mojtaba@hotmail.com>
2022-01-03 16:05:33 +0100

    hasCircle() => recursion

--- src/recursion/DemoOrt.java
+++ src/recursion/DemoOrt.java
@@ -1,4 +1,30 @@
 package recursion;
 
+import java.util.Arrays;
+
 public class DemoOrt {
+    public static void main(String[] args) {
+        Ort graz = new Ort("graz");
+        Ort wien = new Ort("wien");
+        Ort marburg = new Ort("marburg");
+        Ort linz = new Ort("linz");
+        Ort tirol = new Ort("tirol");
+
+        graz.addNeighbour(marburg);
+        graz.addNeighbour(wien);
+
+        wien.addNeighbour(linz);
+
+        linz.addNeighbour(graz);
+        linz.addNeighbour(wien);
+
+        marburg.addNeighbour(graz);
+        wien.addNeighbour(graz);
+
+
+        System.out.println(graz.hasCircle(graz));
+        System.out.println("graz: " + graz.getValidCircle());
+    }
+
+
 }
--- src/recursion/Ort.java
+++ src/recursion/Ort.java
@@ -1,4 +1,47 @@
 package recursion;
 
+import java.util.ArrayList;
+
 public class Ort {
+    private String name;
+    private ArrayList<Ort> neighbours = new ArrayList<>();
+    private ArrayList<String> validCircle = new ArrayList<>();
+
+    public Ort(String name) {
+        this.name = name;
+    }
+
+    public void addNeighbour(Ort o) {
+        neighbours.add(o);
+    }
+
+    public boolean hasCircle(Ort o) {
+        return hasCircle(o, o, null);
+    }
+
+    public boolean hasCircle(Ort o, Ort current, Ort previous) {
+        validCircle.add(current.name);
+        for (Ort neighbour : current.neighbours) {
+        if (neighbour == previous) {
+            validCircle.clear();
+            return false;
+        }
+        if (neighbour == o){
+            validCircle.add(o.name);
+            return true;
+        } else {
+            if (hasCircle(o,neighbour,current))
+            return true;
+            }
+        }
+        return false;
+    }
+
+    public ArrayList<Ort> getNeighbours() {
+        return neighbours;
+    }
+
+    public ArrayList<String> getValidCircle() {
+        return validCircle;
+    }
 }
